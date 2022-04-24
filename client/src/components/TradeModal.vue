31715d98ab2d47aeb8220585d057a83220bf9085
Rinku Chaudhari <rinku.chaudhary@amniltech.com>
2022-04-24 18:12:21 +0545

    issue while current total price exceeds total balance fix

--- client/src/components/TradeModal.vue
+++ client/src/components/TradeModal.vue
@@ -27,7 +27,7 @@
         <button
           class="main-action-button"
           @click="this.previewMode = true"
-          :disabled="quantity<=0 || currentMode==='Sell'&&quantity>noOfCoins"
+          :disabled="quantity<=0 || currentMode==='Sell'&&quantity>noOfCoins || currentMode==='Buy' && (quantity*pricePerCoin)>totalBalance"
           :style="[currentMode === 'Sell' ? { background: 'coral' } : {}]"
         >
           Preview {{ currentMode }}
